9de984e973b0229d4ae2fbcc826c918af265b535
Savannah Acevedo <140823774+savvycolleenusers.noreply.github.com>
2023-09-14 16:41:23 -0400

    added css and js folders with files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const url = "http://shibe.online/api/shibes?count=[1-100]"
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector("img").src= data[0]
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
